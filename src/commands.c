3d5fe18b53d70ea73302906dd377c0f85c4188a0
Ben Mezger <me@benmezger.nl>
2017-07-09 15:28:16 -0300

    commands.c: Write content to binary Fat-16.

--- src/commands.c
+++ src/commands.c
@@ -1,3 +1,5 @@
+#include <unistd.h>
+#include <string.h>
 #include <stdlib.h>
 #include "commands.h"
 #include "fat16.h"
@@ -13,7 +15,56 @@ struct fat_dir *ls(FILE *fp, struct fat_bpb *bpb){
     return dirs;
 }
 
-void mv(FILE *fp, char *filename, char *dst, struct fat_bpb *bpb){
-    ;; // TODO
+int write_dir(FILE *fp, char *fname, struct fat_dir *dir){
+    strncpy((char *) dir->name, (char *) fname, sizeof(fname));
+
+    if (fwrite(dir, 1, sizeof(struct fat_dir), fp) <= 0)
+        return -1;
+    return 0;
+}
+
+int write_data(FILE *fp, char *fname, struct fat_dir *dir, struct fat_bpb *bpb){
+
+    FILE *localf = fopen(fname, "r");
+    int *c;
+    int start_offset = bpb_froot_addr(bpb) + (bpb->bytes_p_sect * \
+            dir->first_clust_low);
+
+    while ((*c = fgetc(localf)) != EOF){
+        fseek(fp, start_offset, SEEK_SET);
+        if (fwrite((char *) c, 1, sizeof(c), fp) != sizeof(c))
+            return -1;
+        start_offset += 1;
+    }
+    return 0;
+}
+
+void mv(FILE *fp, char *filename, struct fat_bpb *bpb){
+    if (access(filename, F_OK) < 0)
+        return;
+    struct fat_dir *dirs = ls(fp, bpb); // find empty place to store file.
+    // fprintf(stdout, "%d\n", bpb_fdata_addr(bpb) + bpb->bytes_p_sect);
+    int dirs_len = sizeof (struct fat_dir) * bpb->possible_rentries;
+
+    int i;
+    uint32_t data_addrs = bpb_froot_addr(bpb);
+    struct fat_dir *curdir;
+
+    for (i=0; i < dirs_len; i++){
+        data_addrs += bpb->bytes_p_sect;
+        if (dirs[i].name[0] == '\0'){
+            curdir = &dirs[i]; /* found the first free directory entry */
+            curdir->first_clust_low = i + 1;
+            break;
+        }
+    }
+    //fprintf(stdout, "%d\n", data_addrs);
+    int dir_addr = bpb_froot_addr(bpb) + i * 32;
+    fseek(fp, dir_addr, SEEK_SET);
+
+    if (write_dir(fp, filename, curdir) < 0)
+        return;
+    if (write_data(fp, filename, curdir, bpb) < 0)
+        return;
 }
 
--- src/commands.h
+++ src/commands.h
@@ -6,7 +6,13 @@
 /* list files in fat_bpb */
 struct fat_dir *ls(FILE *, struct fat_bpb *);
 
+/* write content to directory */
+int write_dir (FILE *, char *, struct fat_dir *);
+
+/* write file content to fat directory */
+int write_data(FILE *, char *, struct fat_dir *, struct fat_bpb *);
+
 /* move file from source to destination */
-void mv(FILE *, char *, char *, struct fat_bpb *);
+void mv(FILE *, char *, struct fat_bpb *);
 
 #endif
